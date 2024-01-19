04cd62b6d54b54a63aaa0d8a5f4c2d53805e4d4d
a-committed-dev <winther.dev@gmail.com>
2024-01-19 23:41:11 +0100

    new plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,23 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate"},
+  { import = "astrocommunity.indent.mini-indentscope"},
+  -- { -- further customize the options set by the community
+  --   "mini-indentscope",
+  --   delay = 300,
+  -- },
+  { import = "astrocommunity.editing-support.todo-comments-nvim"},
+  { import = "astrocommunity.editing-support.auto-save-nvim"},
+  { import = "astrocommunity.syntax.vim-sandwich"},
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
+
+
