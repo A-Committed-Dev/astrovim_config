75f85c3a7a80d3b400364b126802596e70020672
a-committed-dev <winther.dev@gmail.com>
2024-03-07 16:57:33 +0100

    updated themeing and some extra plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -11,8 +11,9 @@ return {
   --   delay = 300,
   -- },
   { import = "astrocommunity.editing-support.todo-comments-nvim"},
-  { import = "astrocommunity.editing-support.auto-save-nvim"},
+  -- { import = "astrocommunity.editing-support.auto-save-nvim"},
   { import = "astrocommunity.syntax.vim-sandwich"},
+  
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
